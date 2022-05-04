d2126a882efd4c091624fb5b9e89fac5476f6e56
felipe <an-felipe@hotmail.es>
2022-05-04 12:54:31 +0200

    changes

--- src/Components.js/TaskContainer.js
+++ src/Components.js/TaskContainer.js
@@ -7,52 +7,40 @@ export const TaskContainer = ()=>{
      
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        
 
     }
+    const taskStyles = {
+     
+        border: 'solid 1px #eee',
+        boxShados: '0 5px 5px rgb(0, 0, 0, 0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px',
+    }
+    const testTask = [
+        {name: 'comprar huevos', info: 'lalala'},
+        {name: 'comprar leche', info: 'lalala'},
+        {name: 'llamar', info: 'lalala'},
 
+    ]
   
     return(
-      <div >
+      <div style={tasks} >
+       
+        <div className="taskStyles">
+            Hola mundo
+        </div>
+        <div className="taskStyles">
+            Hola mundo
+        </div>
+      
 
 
-             <div style={tasks}>
-              
-              <Task/>
-              
-          
-             </div>
-            <div style={tasks}>
-            
-            <Task/>
-            
-        
-            </div>
-
-            <div style={tasks}>
-            
-            <Task/>
-            
-        
-            </div>
-
-
-            <div style={tasks}>
-            
-            <Task/>
-            
-        
-            </div>
-
-            <div style={tasks}>
-            
-            <Task/>
-            
-        
-            </div>
-
+           
 
     </div>
+    
 
              
     )
