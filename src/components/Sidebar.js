617222860f48e37fdedb1c06990fbe6e9498e68d
Carlos <carlos@Mac-mini-de-Innobing.local>
2022-05-12 12:44:52 +0200

    Inicio de diseño para tareas y push antes de merge

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,42 +1,85 @@
 import React from "react";
-import './Sidebar.css';
-
-// No se puede 
+import { List, ListItemButton, ListItemIcon, ListItemText, Collapse, Card, CardContent, Typography } from "@mui/material";
+import { AccountCircle, ExpandLess, ExpandMore, Info, Assignment } from "@mui/icons-material";
 
 const SideBar = () => {
 
+    const [open, setOpen] = React.useState(false);
+
+    const openFilters = () => {
+        setOpen(!open);
+    };
+
+    const cardStyle = {
+        width: '30px',
+        height: '20px',
+        fontSize: 14,
+        textAlign: 'center'
+    }
+
+    const contentAll = {
+        float: 'left',
+        height: '100%',
+        width: '100%',
+        paddingTop: '40px',
+        backgroundColor: 'white'
+    }
+
+    const titleStyle = {
+        fontSize: '40px',
+        paddingLeft: '40px'
+    }
+
     return (
-        <div className="container-all">
-            <h1>Listapp</h1>
-            <div className="content-list">
-                <ul>
-                    <li>
-                        <a href="#">My account</a>
-                    </li>
-                    <li>
-                        <a href="#">My Tasks</a>
-                        <div className="dropdown">
-                            <button className="dropbtn">Dropdown</button>
-                            <div id="myDropdown" className="dropdown-content">
-                                <ul>
-                                    <li>
-                                        <a href="#">Recent</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">In Progress</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Completed</a>
-                                    </li>
-                                </ul>  
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                </ul>
-            </div>
+        <div  style={contentAll}>
+            <h1 style={titleStyle}>Listapp</h1>
+            <List
+                sx={{ width: '100%', maxWidth: 300, bgcolor: 'background.paper' }}
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+            >
+                <ListItemButton>
+                    <ListItemIcon>
+                        <AccountCircle />
+                    </ListItemIcon>
+                    <ListItemText primary="My account" />
+                </ListItemButton>
+                <ListItemButton onClick={openFilters}>
+                    <ListItemIcon>
+                        <Assignment />
+                    </ListItemIcon>
+                    <ListItemText primary="My Tasks" />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton sx={{ pl: 6 }}>
+                            <ListItemText primary="Recents" />
+                            <Card style={cardStyle}>
+                                12
+                            </Card>
+                        </ListItemButton>
+                        <ListItemButton sx={{ pl: 6 }}>
+                            <ListItemText primary="In Progress" />
+                            <Card style={cardStyle}>
+                                8
+                            </Card>
+                        </ListItemButton>
+                        <ListItemButton sx={{ pl: 6 }}>
+                            <ListItemText primary="Completed" />
+                            <Card style={cardStyle}>
+                                4
+                            </Card>
+                        </ListItemButton>
+                    </List>
+                </Collapse>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <Info />
+                    </ListItemIcon>
+                    <ListItemText primary="About" />
+                </ListItemButton>
+            </List>
         </div>
     )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { Card, CardContent, Typography } from "@mui/material";
 
 export const Task= ()=>{
+
     const taskStyles = {
-     
         border: 'solid 1px #eee',
         boxShadow: '5px 5px 5px rgb(0, 0, 0, 0.1)',
         minWidth: '28%',
@@ -13,10 +13,16 @@ export const Task= ()=>{
         backgroundColor:'white',
     }
     return(
-      
-        <div style={taskStyles}>
-            <h3>Tarea principal</h3>
-            <p> prueba </p>
-        </div>
+        <Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
